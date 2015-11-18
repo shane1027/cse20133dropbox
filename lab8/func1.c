70d1278b1176895f81fe55fa74faa6a96511fcf8
shane1027 <shanend27@gmail.com>
2015-11-17 20:52:12 -0500

    added comments, moving to test as independent function

--- lab8/func1.c
+++ lab8/func1.c
@@ -8,27 +8,35 @@ int main()
     int j = 0;
     int SIZE = 0;
     FILE * music;
-    int * music_data;
+    char * music_data;
 
+    // open file and return error if not found
     music = fopen(FILENAME, "r");
     if (music == NULL) {
         printf("File not found.\n");
         return EXIT_FAILURE;
     }
 
+    // increment the SIZE variable for every character
+    // added: other than whitespace
     while (fgetc(music) != EOF) {
        SIZE++; 
     }
-    printf("%d\n", SIZE);
+    printf("%d\n", SIZE); // remove after debugging
 
-    music_data = calloc(SIZE, sizeof(int));
+    // now we can allocate an array to store the music data efficiently
+    // (in terms of memory)
+    music_data = calloc(SIZE, sizeof(char));
 
+    // remove after debugging, filling array to print
     for (j=0; j<SIZE; j++) {
         music_data[j] = j;
         printf("\n%d", music_data[j]);
     }
 
     printf("\n");
+    
+    // always free memory and close files!!
     fclose(music);
     free(music_data);
 
